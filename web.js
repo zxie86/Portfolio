ca9eba06c95456bfbf5ef1c25cdbfe92252ccc0f
apple <apple@kel-MacBook.local>
2019-09-22 16:21:45 -0400

    initial commit

---
+++ web.js
@@ -0,0 +1,17 @@
+var count;
+var index;
+var letter = "";
+const text = ' at Western University ';
+index = 0;
+(function type(){
+    if(index < text.length){
+        letter = text.slice(0,++index);
+    }else{
+        index = 0;
+    }
+    
+    document.getElementById('typing').innerHTML = letter;
+    setTimeout(type,130);
+
+   
+}());
